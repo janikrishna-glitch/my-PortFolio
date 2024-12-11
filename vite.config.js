84ffad9825c287cb5d4dcc5b3533378a1c7f4a42
Jani Krishna <janikrishna305@gmail.com>
2024-12-11 16:58:34 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      external: ['/assets/index-7ad8cd00.js']
+   
     }
   }
-});
+);
 
